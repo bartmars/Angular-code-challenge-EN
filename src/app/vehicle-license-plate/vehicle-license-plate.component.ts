b0cb5a6ec70c949c91de5be6d235eb42d3e666db
Bart Mars <bart.mars@outlook.com>
2025-05-13 12:59:30 +0200

    update non-working component

--- src/app/vehicle-license-plate/vehicle-license-plate.component.ts
+++ src/app/vehicle-license-plate/vehicle-license-plate.component.ts
@@ -1,36 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { KentekenCheck } from 'rdw-kenteken-check';
+import { Component, OnInit } from '@angular/core'
+import { KentekenCheck } from 'rdw-kenteken-check'
+import { FormsModule } from '@angular/forms'
 
 @Component({
     selector: 'app-vehicle-license-plate',
     templateUrl: './vehicle-license-plate.component.html',
     styleUrls: ['./vehicle-license-plate.component.css'],
-    standalone: false
+    standalone: false,
 })
 export class VehicleLicensePlateComponent implements OnInit {
   
-  // Does not work
-  // Error: src/app/vehicle-license-plate/vehicle-license-plate.component.ts:19:51 - error TS2345: Argument of type 'HTMLElement' is not assignable to parameter of type 'HTMLInputElement'.
-  // Type 'HTMLElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, autocomplete, and 52 more.
-  // kenteken: string = '45-HNT-1'
-  // message: string = ''
+  /* 
+  JavaScript validatie oplossing
+Deze kentekenCheck is gebaseerd op de actuele formats(kentekencombinaties) welke zijn uitgegeven door de RDW(Rijksdienst voor het Wegverkeer) 
+en welke zijn te vinden via bijgevoegde link. De open data API vd RDW retourneert geen koppeltekens in het kenteken voor zover bekend, dus 
+vandaar deze oplossing. De array van regex patronen correspondeert met de lijst van formats op de site vd RDW in bijgaande link. De functie 
+'kentekenCheck' controleert of het ingevoerde kenteken correspondeert met het eerste valide patroon en retourneert vervolgens true en
+formatteert het kenteken. Indien false retourneert default 'XX-XX-XX', of een custom string, er worden in de latere series geen klinkers 
+gebruikt en geen karakters die de RDW voorschrijft. Kentekens met AA(Koningshuis) en CD(Corps Diplomatique) zijn in deze functie niet meegenomen, 
+de letters C en Q mogen niet meer vd overheid ivm interpretatie problemen en zijn wel meegenomen.
 
-  inputElm: HTMLElement = document.getElementById('input-kenteken') as HTMLInputElement
-  outputElm: HTMLElement|null = document.getElementById('output-kenteken')
+Verboden combinaties: GVD, KKK, KVT, LPF, NSB, PKK, PSV, TBS, SS en SD (ook niet in lettercombinaties met 3 letters) Verboden vanaf serie 11: PVV, SGP, VVD, FVD, BBB
 
+De rdw toont momenteel alleen serie 6 t/m 10 op hun website https://www.rdw.nl/particulier/voertuigen/auto/de-kentekenplaat/het-kenteken-op-de-plaat/uitleg-over-de-cijfers-en-letters-op-de-kentekenplaat
 
-  // constructor() {
-  //   try {
-  //     const kt = new KentekenCheck(this.kenteken, this.inputElm, this.outputElm)
-  //     kt.formatLicense()
-  //     kt.bindInputListener()
-  //   }
-  //   catch (e) {
+Alle series voor personenauto's zijn hier zichtbaar https://nl.m.wikipedia.org/wiki/Nederlands_kenteken
+  */
+  inputElm = document.getElementsByName('kenteken')
 
+  kenteken: string = ''
+  
+  handleClick() {
+    console.log(this.inputElm)
+  }
 
-  //     console.log(e)
-  //   }
-  // }
+  verifyLicensePlate() {
+    // const kt = new KentekenCheck(inputElm.value)
+
+    // const kt = new KentekenCheck('S007JB');
+    // const kt = new KentekenCheck(this.kenteken);
+    // alert(kt.formatLicense())
+    const inputElm = document.getElementById('myInput') as HTMLInputElement
+    let value = inputElm.value
+
+    console.log(value)
+
+  }
 
   ngOnInit(): void {
   }
